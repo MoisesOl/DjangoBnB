a1d31ef6e3ccb16492c4f41eac66d3a194cdc465
MoisesOl <olivaresoviedo99@gmail.com>
2024-08-14 16:26:26 -0400

    Preparación del proyecto para agregar funcionalidad de inbox

--- backend/djangobnb_backend/entrypoint.sh
+++ backend/djangobnb_backend/entrypoint.sh
@@ -1,18 +1,25 @@
 #!/bin/sh
 
-if [ "$DATABASE" = "postgres" ]; then
-    echo "Check if database is running..."
+echo "Script started..."
 
-    while ! nc -z $SQL_HOST $SQL_PORT; do
+if [ "$DATABASE" = "postgres" ]; then
+    echo "Checking if database is running..."
+
+    while ! nc -z "$SQL_HOST" "$SQL_PORT"; do
+        echo "Waiting for database..."
         sleep 0.1
     done
 
-    echo "The database is up and running :D"
+    echo "Database is up and running :D"
 fi
 
-
+echo "Running migrations..."
 python manage.py makemigrations
+
+echo "Applying migrations..."
 python manage.py migrate
+
+echo "Starting server..."
 python manage.py runserver 0.0.0.0:8000
 
 exec "$@"
